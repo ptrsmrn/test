a0799a23f5bcde18242d8ffdc291228598849512
Piotr Smaroń <piotr.smaron@scylladb.com>
2023-04-02 23:22:21 +0200

    Fix returning a value from entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/bash -l
 
 start_time=`date +%s`
 
 python /gh_project_automation.py --project-name Eliran --team Eliran-team -l DEBUG $PAT
 
 end_time=`date +%s`
-echo "The script run for: $((end_time-start_time))" >> $GITHUB_OUTPUT
+echo "The script run for $((end_time-start_time))s" >> $GITHUB_OUTPUT
