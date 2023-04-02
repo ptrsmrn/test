2e4967b5e9a1434646bac8d000ee403c72c2a500
Piotr Smaroń <piotr.smaron@scylladb.com>
2023-04-02 22:12:21 +0200

    Try passing PAT by docker's env var

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-echo "$@"
-echo "$1"
-python /gh_project_automation.py --project-name Eliran --team Eliran-team -l DEBUG $1
+python /gh_project_automation.py --project-name Eliran --team Eliran-team -l DEBUG $PAT
