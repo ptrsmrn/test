b40a6c94fe3279e0e20a1bd3b4683ce3649a24d2
Piotr Smaroń <piotr.smaron@scylladb.com>
2023-04-02 23:34:27 +0200

    remove unused outputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/bash -l
 
-start_time=`date +%s`
-
 python /gh_project_automation.py --project-name Eliran --team Eliran-team -l DEBUG $PAT
 
-end_time=`date +%s`
-echo "The script run for $((end_time-start_time))s" >> $GITHUB_OUTPUT
